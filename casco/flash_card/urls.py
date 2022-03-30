ca2f9b28b3f1863ad025979b0431c28dcc3a5058
Ali <Alihosseinzadeh1379ahz@gmail.com>
2022-03-31 00:14:56 +0430

    view/url/template added for DeckDetialView - tiny modification in deck_list.html

--- casco/flash_card/urls.py
+++ casco/flash_card/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import DeckListView
+from .views import DeckListView, DeckDetailView
 
 
 app_name = 'flash_card'
 
 urlpatterns = [
     path('decks/', DeckListView.as_view(), name='deck-list'),
+    path('decks/<int:pk>/', DeckDetailView.as_view(), name='deck-detail'),
 ]
--- casco/flash_card/views.py
+++ casco/flash_card/views.py
@@ -1,7 +1,24 @@
 from .models import Deck, Card
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class DeckListView(ListView):
     template_name = 'deck_list.html'
     queryset = Deck.objects.order_by('-updated_at')
+
+
+class DeckDetailView(DetailView):
+    template_name = 'deck_detail.html'
+    model = Deck
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        d1 = Deck.objects.get(pk=self.kwargs.get('pk')) 
+        context['card_list'] = d1.card_set.all()
+        return context
+
+    def get_object(self):
+        deck = super().get_object()
+        deck.views_count += 1
+        deck.save()
+        return deck
